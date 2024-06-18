59e11b931f90c0ee0f08b017d206a3d30ee4c0c5
javier <rampxxxx@gmail.com>
2024-06-18 19:46:30 +0200

    gopls. Activate inlay hints in gopls
    - Toogle with <space>ti (leader+Telescope+Inlay)
    
    Signed-off-by: javier <rampxxxx@gmail.com>

--- init.lua
+++ init.lua
@@ -615,7 +615,17 @@ require("lazy").setup({
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				-- clangd = {},
-				gopls = {},
+        gopls = {
+          settings = {
+            gopls = {
+              ['ui.inlayhints.hints'] = {
+                compositeLiteralFields = true,
+                constantValues = true,
+                parameterNames = true,
+              },
+            },
+          },
+        },
 				bashls = {},
 				pyright = {},
 				rust_analyzer = {},
