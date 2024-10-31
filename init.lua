3e454a4ac4b2c8a2ca471977b130f6d188c44bfa
Javier Garcia <javier.martin.garcia@ibm.com>
2024-10-31 13:04:18 +0100

    Change which-key old style config to new .

--- init.lua
+++ init.lua
@@ -353,19 +353,17 @@ require("lazy").setup({
             require("which-key").setup()
 
             -- Document existing key chains
-            require("which-key").register({
-                ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-                ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-                ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-                ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-                ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-                ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-                ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+            local wk = require("which-key")
+            wk.add({
+                { "<leader>c", desc = "[C]ode" },
+                { "<leader>d", desc = "[D]ocument" },
+                { "<leader>r", desc = "[R]ename" },
+                { "<leader>s", desc = "[S]earch" },
+                { "<leader>w", desc = "[W]orkspace" },
+                { "<leader>t", desc = "[T]oggle" },
+                { "<leader>h", desc = "Git [H]unk" },
+                { "<leader>h", desc = "Git [H]unk", mode = "v" },
             })
-            -- visual mode
-            require("which-key").register({
-                ["<leader>h"] = { "Git [H]unk" },
-            }, { mode = "v" })
         end,
     },
 
