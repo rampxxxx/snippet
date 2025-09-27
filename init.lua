708fcb6d89c74b007f6c5ffc32c6e80e01301a4a
javier <rampxxxx@gmail.com>
2025-09-27 18:34:22 +0200

    Comment out OSC52 by now , it's a bit annoying.
    Signed-off-by: javier <rampxxxx@gmail.com>

--- init.lua
+++ init.lua
@@ -1108,21 +1108,21 @@ require("lazy").setup({
     },
 })
 
--- INI USC52
-local g = vim.g
-
-g.clipboard = {
-	name = "OSC 52",
-	copy = {
-		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-	},
-	paste = {
-		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-	},
-}
--- END USC52
+-- INI OSC52
+-- local g = vim.g
+--
+-- g.clipboard = {
+-- 	name = "OSC 52",
+-- 	copy = {
+-- 		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+-- 		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+-- 	},
+-- 	paste = {
+-- 		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+-- 		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+-- 	},
+-- }
+-- END OSC52
 
 -- INIT : rose-pine config and load in that order
 require("rose-pine").setup({
